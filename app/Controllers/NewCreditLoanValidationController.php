3c957e903738808fedc8103bdc2bc696f443694f
JaniisKn <Janis_Kneubuehler@sluz.ch>
2020-03-11 13:35:09 +0100

    Connection to DB

--- app/Controllers/NewCreditLoanValidationController.php
+++ app/Controllers/NewCreditLoanValidationController.php
@@ -1,1 +1,9 @@
 <?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+
+    $creditloan = new Creditloan(null, "Janis","Kneubühler", false, "janis.kne@gmail.com", "079 105 31 78", 4, "2020-05-06", 12, 1);
+    $creditloan->create();
+
+    //header('Location: creditloan'); // Besser: header('Location: http://localhost/deinProjekt/task);
+}
+?>
