6ff8b15201176688cb32606176aa1e1ee05a8031
Raphael Härtel <raphael.haertel@bluewin.ch>
2020-03-11 11:34:23 +0100

    WelcomeView

---
+++ app/Views/NewCreditLoan.View.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="de">
+    <head>
+        <meta charset="UTF-8">
+        <title>Kredihay</title>
+        <link rel="icon" href="res/kredithay.png">
+        <link rel="stylesheet" href="public/css/app.css">
+    </head>
+    <body>
+    <h1>Kredihay</h1>
+
+    <h2>Kreditverleih</h2>
+
+    <table>
+        <tr>
+            <th>Name</th>
+            <th>Kreditart</th>
+            <th>Zahlungsfrist</th>
+            <th>Status</th>
+            <th>Bearbeiten</th>
+        </tr>
+    </table>
+
+    </body>
+</html>
